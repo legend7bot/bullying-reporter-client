a0b3a13ea830d8b57245c56146f95d2e23f5bf29
Himanshu <him.raw.official1@gmail.com>
2022-09-18 01:17:22 +0530

    login page done
    report page basic done (without styling)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from './pages/Home'
 import Report from './pages/Report';
 import {Route, Routes} from "react-router-dom";
+import Login from './pages/Login';
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/report" element={<Report />} />
+        <Route path="/login" element={<Login />} />
         <Route  path="/*" element={"error 404 page not found sowwy"} />
       </Routes>
     </>
--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 function Report() {
   return (
-    <div>Report</div>
+    <>
+      <div className="report-body">
+        <div className="main-report">
+        <form>
+          <label htmlFor="name">name</label>
+          <input type="text" name='name' />
+          <br />
+          <label htmlFor="email">email</label>
+          <input type="email" name='email' />
+          <br />
+          <label htmlFor="contact">contact</label>
+          <input type="contact" name='contact' />
+          <br />
+          <label htmlFor="location">location</label>
+          <input type="location" name='location' />
+          <br />
+          <label htmlFor="description">description</label>
+          <textarea name='description'/>
+          <br />
+          <label htmlFor="accusedDetails">Accused Details</label>
+          <textarea name='accusedDetails'/>
+          <br />
+        </form>
+        </div>
+      </div>
+    </>
   )
 }
 
