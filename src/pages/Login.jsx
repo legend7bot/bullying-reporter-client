9b0ed02f086efa1ae1345204813119e63195ae4f
Himanshu <him.raw.official1@gmail.com>
2022-09-18 12:19:51 +0530

    login form done report styling done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
 import "./Login.css"
 function Login() {
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+      });
+      function handleChange(event) {
+        const { name, value } = event.target;
+        setData((prevValue) => {
+          return ({ ...prevValue, [name]: value })
+        });
+      }
     return (
         <>
             <div className="login-body">
-                <div class="container1">
-                    <div class="screen">
-                        <div class="screen__content">
-                            <form class="login">
-                                <div class="login__field">
-                                    <i class="login__icon fas fa-user"></i>
-                                    <input type="text" class="login__input" placeholder="User name / Email" />
+                <div className="container1">
+                    <div className="screen">
+                        <div className="screen__content">
+                            <form className="login">
+                                <div className="login__field">
+                                    <i className="login__icon fas fa-user"></i>
+                                    <input type="text" className="login__input" name="email" onChange={handleChange} placeholder="User name / Email" />
                                 </div>
-                                <div class="login__field">
-                                    <i class="login__icon fas fa-lock"></i>
-                                    <input type="password" class="login__input" placeholder="Password" />
+                                <div className="login__field">
+                                    <i className="login__icon fas fa-lock"></i>
+                                    <input type="password" className="login__input" name='password' onChange={handleChange} placeholder="Password" />
                                 </div>
-                                <button class="button login__submit">
-                                    <span class="button__text">Log In Now</span>
-                                    <i class="button__icon fas fa-chevron-right"></i>
+                                <button className="button login__submit" onClick={(event)=>{
+                                    console.log(data);
+                                    event. preventDefault();
+                                    }}>
+                                    <span className="button__text">Log In Now</span>
+                                    <i className="button__icon fas fa-chevron-right"></i>
                                 </button>
                             </form>
-                            <div class="social-login">
+                            <div className="social-login" >
                                 <h3>log in</h3>
                             </div>
                         </div>
-                        <div class="screen__background">
-                            <span class="screen__background__shape screen__background__shape4"></span>
-                            <span class="screen__background__shape screen__background__shape3"></span>
-                            <span class="screen__background__shape screen__background__shape2"></span>
-                            <span class="screen__background__shape screen__background__shape1"></span>
+                        <div className="screen__background">
+                            <span className="screen__background__shape screen__background__shape4"></span>
+                            <span className="screen__background__shape screen__background__shape3"></span>
+                            <span className="screen__background__shape screen__background__shape2"></span>
+                            <span className="screen__background__shape screen__background__shape1"></span>
                         </div>
                     </div>
                 </div>
--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,29 +1,43 @@
 import React from 'react'
-
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Button } from '@mui/material';
 function Report() {
   return (
     <>
       <div className="report-body">
+      <h1 className='report-heading'>COMPLAINT DETAILS</h1>
         <div className="main-report">
         <form>
-          <label htmlFor="name">name</label>
-          <input type="text" name='name' />
+          <div className="row report-form">
+            <label htmlFor="name" className='col-5 report-label' >name <span className="required-start">*</span> </label>
+          <input type="text" name='name'  className='col-7 form-control report-form-fields' placeholder='Your name'/>
           <br />
-          <label htmlFor="email">email</label>
-          <input type="email" name='email' />
+          <label htmlFor="email" className='col-5 report-label' >email <span className="required-start">*</span> </label>
+          <input type="email" name='email'  className='col-7 form-control report-form-fields' placeholder='Your email'/>
           <br />
-          <label htmlFor="contact">contact</label>
-          <input type="contact" name='contact' />
+          <label htmlFor="contact" className='col-5 report-label' >contact <span className="required-start">*</span> </label>
+          <input type="contact" name='contact'  className='col-7 form-control report-form-fields' placeholder='Your contact no.'/>
           <br />
-          <label htmlFor="location">location</label>
-          <input type="location" name='location' />
+          <label htmlFor="location" className='col-5 report-label' >location <span className="required-start">*</span> </label>
+          <input type="location" name='location'  className='col-7 form-control report-form-fields' placeholder='The location of bullying'/>
           <br />
-          <label htmlFor="description">description</label>
-          <textarea name='description'/>
+          <label htmlFor="description" className='col-5 report-label' >description <span className="required-start">*</span> </label>
+          <textarea name='description' className='col-7 form-control report-form-fields' placeholder='Description' rows={4} />
           <br />
-          <label htmlFor="accusedDetails">Accused Details</label>
-          <textarea name='accusedDetails'/>
+          <label htmlFor="accusedDetails" className='col-5 report-label' >Accused Details <span className="required-start">*</span> </label>
+          <textarea name='accusedDetails' className='col-7 form-control report-form-fields' placeholder='Details of the person accused' rows={3} />
           <br />
+          </div>
+          <a href="/">
+<Button variant="outlined" startIcon={<ArrowBackIosIcon/>} >
+            Home Page
+          </Button>
+          </a>
+          
+          <Button variant="contained" endIcon={<NavigateNextIcon/>} sx={{position:"absolute",right:"19%"}}>
+            Send
+          </Button>
         </form>
         </div>
       </div>
