5910f5c2e181d76681c975021d71a13accf6a8c2
Harsh Vardhan Sharma <38874238+legend7bot@users.noreply.github.com>
2022-09-18 13:56:09 +0530

    added-api

--- src/features/auth/authService.jsx
+++ src/features/auth/authService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/';
+const API_URL = 'https://bullying-reporter.herokuapp.com/api/';
 
 // Login user
 const login = async (userData) => {
